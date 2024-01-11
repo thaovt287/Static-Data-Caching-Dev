95254fa4105a23e0354c6a81ffebaf94ae8cfb25
AnhMTV <maithachvietanh@gmail.com>
2024-01-11 17:02:28 +0700

    [Issue-1] Update basic logic fetching and save file from block chain

--- src/contants.ts
+++ src/contants.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
 
-export const CHAIN_LIST: string[] = ['polkadot', 'kusama']
+export const CHAIN_LIST: string[] = ['polkadot', 'kusama', 'acala']
 // Export current folder /data
 export const DATA_DIR = path.join(__dirname, '../data')
